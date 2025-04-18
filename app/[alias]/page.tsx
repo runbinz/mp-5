e498c9514a037ce25902182bbba5034b348741a0
Runbin Zhou <runbin1000@gmail.com>
2025-04-18 13:32:58 -0400

    remove await

--- app/[alias]/page.tsx
+++ app/[alias]/page.tsx
@@ -4,7 +4,7 @@ import getLink from "@/lib/getLink";
 export default async function RedirectAlias({params}: {
     params: { alias: string };
 }) {
-    const { alias } = await params;
+    const { alias } = params;
 
     const url = await getLink(alias);
     if (url === null) {
